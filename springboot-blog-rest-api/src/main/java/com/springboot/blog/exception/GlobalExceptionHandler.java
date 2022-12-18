1ddb95ef3a0ddd901a9bafb38c99ad8fb6b1c4c6
eunbi <vysql123@naver.com>
2022-12-18 16:42:23 +0900

    Spring Validation

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
@@ -20,6 +20,20 @@ public class GlobalExceptionHandler {
                 webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
-    // global exceptions
 
+    @ExceptionHandler(BlogAPIException.class)
+    public ResponseEntity<ErrorDetails> handleBlogAPIException(BlogAPIException exception,
+                                                                        WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+    // global exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception,
+                                                               WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
